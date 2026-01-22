98d031a59bc4482d1f07a126cfeae98981392765
Ramy Hakeem <ramyhakeem07@gmail.com>
2026-01-22 10:44:44 +0200

    feat: Enhance profile update functionality with country selection and validation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,12 +1,20 @@
 "use client";
-function UpdateProfileForm({ SelectCountry }) {
+
+import { useFormStatus } from "react-dom";
+import { updateGuest } from "../_lib/actions";
+function UpdateProfileForm({ children, guest }) {
+  const { countryFlag, email, nationalID, fullName } = guest;
   // CHANGE
-  const countryFlag = "pt.jpg";
   return (
-    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+    <form
+      action={updateGuest}
+      className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+    >
       <div className="space-y-2">
         <label>Full name</label>
         <input
+          defaultValue={fullName}
+          name="fullName"
           disabled
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -15,6 +23,8 @@ function UpdateProfileForm({ SelectCountry }) {
       <div className="space-y-2">
         <label>Email address</label>
         <input
+          defaultValue={email}
+          name="email"
           disabled
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
         />
@@ -23,30 +33,41 @@ function UpdateProfileForm({ SelectCountry }) {
       <div className="space-y-2">
         <div className="flex items-center justify-between">
           <label htmlFor="nationality">Where are you from?</label>
-          <img
-            src={countryFlag}
-            alt="Country flag"
-            className="h-5 rounded-sm"
-          />
+          {countryFlag && (
+            <img
+              src={countryFlag}
+              alt="Country flag"
+              className="h-5 rounded-sm"
+            />
+          )}
         </div>
-        {SelectCountry}
+        {children}
       </div>
 
       <div className="space-y-2">
         <label htmlFor="nationalID">National ID number</label>
         <input
+          defaultValue={nationalID}
           name="nationalID"
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-
-      <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
-      </div>
+      <Button>Update profile</Button>
     </form>
   );
 }
 
 export default UpdateProfileForm;
+function Button({ children }) {
+  const { pending } = useFormStatus();
+  return (
+    <div className="flex justify-end items-center gap-6">
+      <button
+        disabled={pending}
+        className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300 "
+      >
+        {pending ? "Updating..." : children}
+      </button>
+    </div>
+  );
+}
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,10 +1,39 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import supabase from "./supabase";
 
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
 export async function signInAction() {
   await signIn("google", { redirectTo: "/" });
+}
+
+export async function updateGuest(formData) {
+  console.log(formData);
+  const session = await auth();
+  if (!session?.user) {
+    throw new Error("Not authenticated");
+  }
+
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+  const nationalID = formData.get("nationalID");
+  const idRegex = /^(?=.*\d)[A-Za-z0-9\- ]{4,25}$/;
+  if (nationalID && !idRegex.test(nationalID)) {
+    throw new Error("Invalid National ID number format");
+  }
+  const { data, error } = await supabase
+    .from("guests")
+    .update({ nationalID, nationality, countryFlag })
+    .eq("id", session.user.id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Guest could not be updated");
+  }
+  revalidatePath("/account/profile");
 }
